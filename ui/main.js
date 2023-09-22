c80fad0ab27e361857844064ac18eaae4b74b643
CP324754 <lutandotumana@capitecbank.co.za>
2023-09-22 19:28:25 +0200

    -Host fix -Pagination fix

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,6 @@
 // Wait for the document to be ready
 $(document).ready(function () {
-    const baseUrl = 'http://localhost:81/';
+    const baseUrl = 'http://ec2-54-89-234-198.compute-1.amazonaws.com:81';
     let users = [];
     let activeUser = {};
     // Function to get users from 'api/user' endpoint and display them in the table
